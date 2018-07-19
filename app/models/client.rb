99ee90f99810217acf649ee0d5c86fdcad1c11d2
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-19 15:19:05 -0400

    Define associations for client model

---
+++ app/models/client.rb
@@ -0,0 +1,7 @@
+class Client < ApplicationRecord
+  has_one :user_account
+  has_many :items
+  has_many :services
+  has_many :merchants, through: :items
+  has_many :merchants, through: :services
+end
