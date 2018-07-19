bb79fffee9ba6a89d8a7863216b473e8b37f11a1
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-19 16:07:20 -0400

    Add UserAccount validations for username and email

--- app/models/user_account.rb
+++ app/models/user_account.rb
@@ -2,4 +2,6 @@ class UserAccount < ApplicationRecord
   belongs_to :merchant, optional: true
   belongs_to :client, optional: true
   has_secure_password
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true
 end
