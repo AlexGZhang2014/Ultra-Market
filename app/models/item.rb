18755fd99c32582b0e8606494b132425f813b374
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-19 18:33:42 -0400

    Add class method scope to find items that are in stock

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,5 @@ class Item < ApplicationRecord
   validates :quantity, numericality: {greater_than_or_equal_to: 0}
   scope :most_recent, -> { order("items.updated_at DESC") }
   scope :oldest, -> { order("items.updated_at ASC") }
+  scope :available, -> { where("items.quantity > ?", 0) }
 end
