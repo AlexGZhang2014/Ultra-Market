e53bb1cacd6756e6ef9ee693548185a3d45d07ab
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-19 22:11:49 -0400

    Define preliminary new, create, and destroy methods for sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+    @user_account = UserAccount.new
+    @user_accounts = UserAccount.all
+  end
+
+  def create
+    @user_account = UserAccount.find(username: params[:username])
+    if @user_account && @user_account.authenticate(params[:password])
+      session[:user_account_id] = @user_account.id
+      redirect_to user_account_path(@user_account)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+end
