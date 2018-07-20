f28520ec59f9ba2a6e81938e11d6d2f495bf423b
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-20 19:18:47 -0400

    Define create action for merchant controller

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,2 +1,7 @@
 class MerchantsController < ApplicationController
+  def create
+    @merchant = current_user.create_merchant(name: current_user.username)
+    redirect_to user_account_path(@merchant.user_account)
+  end
+
 end
