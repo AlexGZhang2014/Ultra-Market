85916f59cfb7c37125693616cf6fcbb33ad9a185
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-20 16:06:27 -0400

    Define strong params for user_account and build its create action

--- app/controllers/user_accounts_controller.rb
+++ app/controllers/user_accounts_controller.rb
@@ -4,5 +4,17 @@ class UserAccountsController < ApplicationController
   end
 
   def create
+    @user_account = UserAccount.new(user_params)
+    if @user_account.save
+      redirect_to user_account_path(@user_account)
+    else
+      render :new
+    end
   end
+
+  private
+
+    def user_params
+      params.require(:user_account).permit(:username, :email, :password)
+    end
 end
