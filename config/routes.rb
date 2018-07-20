cb28987835d32792fb23812c1ff18c252cccce9f
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-19 21:54:28 -0400

    Define routes for all controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "static#home"
+  resources :user_accounts
+  resources :merchants
+  resources :clients
+  resources :items
+  resources :services
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  get "/logout" => "sessions#destroy"
 end
