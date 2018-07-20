4d1798e4ad34de2a41994e5bd638e452ac772b2d
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-20 19:32:33 -0400

    Define create action for client controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,6 @@
 class ClientsController < ApplicationController
+  def create
+    @client = current_user.create_client(name: current_user.username)
+    redirect_to user_account_path(@client.user_account)
+  end
 end
