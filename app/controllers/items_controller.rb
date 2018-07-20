5e69a9e044bfe1f167099da5c4a2fb5324994f1a
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-20 19:53:50 -0400

    Define index, new, create, and show actions for item controller as well as its strong params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,20 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  private
+
+    def item_params
+      params.require(:item).permit(:name, :description)
+    end
 end
