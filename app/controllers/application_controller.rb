1dce750659f552a94568613547eba8fc3e983f12
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-20 19:12:51 -0400

    Create helper methods that deal with logged in status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :current_user
+  before_action :require_login, except: [:new, :create, :home]
+
+  def logged_in?
+    !!session[:user_account_id]
+  end
+
+
+  private
+
+    def require_login
+      redirect_to root_path unless logged_in?
+    end
+
+    def current_user
+      @current_user ||= UserAccount.find(session[:user_account_id]) if session[:user_account_id]
+    end
+
+    helper_method :current_user
 end
