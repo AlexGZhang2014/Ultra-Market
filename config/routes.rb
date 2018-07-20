88d5a9b92a3c3c759df8b9fc9c1908137e321828
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-20 15:42:36 -0400

    Add nested routes for items and services to merchant and client resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static#home"
   resources :user_accounts
-  resources :merchants
-  resources :clients
+  resources :merchants do
+    resources :items, only: [:index, :new, :create, :show]
+    resources :services, only: [:index, :new, :create, :show]
+  end
+  resources :clients do
+    resources :items, only: [:index, :new, :create, :show]
+    resources :services, only: [:index, :new, :create, :show]
+  end
   resources :items
   resources :services
   get "/login" => "sessions#new"
