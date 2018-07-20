b4c682974ebc6613b21117b54c944d3666498c2d
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-20 18:54:59 -0400

    Refactor create action to include flash notice and redirect instead of render new

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user_account = UserAccount.find(username: params[:username])
+    @user_account = UserAccount.find_by(username: params[:username])
     if @user_account && @user_account.authenticate(params[:password])
       session[:user_account_id] = @user_account.id
       redirect_to user_account_path(@user_account)
     else
-      render :new
+      flash[:notice] = "Incorrect username and/or password."
+      redirect_to controller: "sessions", action: "new"
     end
   end
 
