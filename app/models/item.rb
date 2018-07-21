2efff631e211e62f6f3474bfa8c7a8333b327a98
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-21 18:22:59 -0400

    Try making quantity validation work

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :merchant, optional: true
   belongs_to :client, optional: true
-  validates :quantity, numericality: {greater_than_or_equal_to: 0}
+  validates :name, presence: true
+  validates :description, presence: true
+  #validates :quantity, numericality: {greater_than_or_equal_to: 0}
+  validates_numericality_of :quantity, :only_integer => true, :greater_than_or_equal_to => 0
   scope :most_recent, -> { order("items.updated_at DESC") }
   scope :oldest, -> { order("items.updated_at ASC") }
   scope :available, -> { where("items.quantity > ?", 0) }
