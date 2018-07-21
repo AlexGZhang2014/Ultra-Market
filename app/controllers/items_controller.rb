9d1ea28bf2cced952de19597458590727547d5d4
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-20 20:02:04 -0400

    Add logic to index action in item controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,21 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    if params[:merchant_id]
+      @items = Merchant.find(params[:merchant_id]).items
+    else
+      @items = Item.all
+    end
   end
 
   def new
+    @item = Item.new(merchant_id: params[:merchant_id])
   end
 
   def create
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   private
