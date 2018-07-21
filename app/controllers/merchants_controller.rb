e9bf23b36acc95c2b0f2c3e8fcfa209c7def86eb
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-21 17:51:55 -0400

    Attempt making merchant_id usable

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,6 +1,7 @@
 class MerchantsController < ApplicationController
   def create
     @merchant = current_user.create_merchant(name: current_user.username)
+    current_user.save
     redirect_to user_account_path(@merchant.user_account)
   end
 
