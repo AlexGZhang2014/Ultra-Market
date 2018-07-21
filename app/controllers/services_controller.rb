11498f1afa132f196623fb6d7f5f6cb32c51088a
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-20 20:05:58 -0400

    Define index, new, create, and show actions in services controller as well as its strong params to handle nested resources

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,2 +1,26 @@
 class ServicesController < ApplicationController
+  def index
+    if params[:merchant_id]
+      @services = Merchant.find(params[:merchant_id]).services
+    else
+      @services = Service.all
+    end
+  end
+
+  def new
+    @service = Service.new(merchant_id: params[:merchant_id])
+  end
+
+  def create
+  end
+
+  def show
+    @service = Service.find(params[:id])
+  end
+
+  private
+
+    def service_params
+      params.require(:service).permit(:name, :description)
+    end
 end
