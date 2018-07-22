4017c523d5c445c68cf39a77c448593d2c02b7eb
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-21 23:09:31 -0400

    Remove quantity validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,7 @@ class Item < ApplicationRecord
   belongs_to :client, optional: true
   validates :name, presence: true
   validates :description, presence: true
-  #validates :quantity, numericality: {greater_than_or_equal_to: 0}
-  validates_numericality_of :quantity, :only_integer => true, :greater_than_or_equal_to => 0
   scope :most_recent, -> { order("items.updated_at DESC") }
   scope :oldest, -> { order("items.updated_at ASC") }
-  scope :available, -> { where("items.quantity > ?", 0) }
+  scope :available, -> { where(bought: false) }
 end
