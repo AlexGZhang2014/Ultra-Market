f78a09ddf16ed1506922412373e5a8436aceca5c
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-21 21:14:01 -0400

    Define index action in merchants controller

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,4 +1,8 @@
 class MerchantsController < ApplicationController
+  def index
+    @merchants = Merchant.all
+  end
+
   def create
     @merchant = current_user.create_merchant(name: current_user.username)
     #current_user.save
