49910dab9210088bfdd0f208c85937126ff58a5e
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-22 20:20:47 -0400

    Remove #require_login because it disables github login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :current_user
-  before_action :require_login, except: [:new, :create, :home]
-
-  def logged_in?
-    !!session[:user_account_id]
-  end
-
+  #before_action :require_login, except: [:new, :create, :home]
 
   private
+
+    def logged_in?
+      !!session[:user_account_id]
+    end
 
     def require_login
       redirect_to root_path unless logged_in?
@@ -18,5 +17,5 @@ class ApplicationController < ActionController::Base
       @current_user ||= UserAccount.find(session[:user_account_id]) if session[:user_account_id]
     end
 
-    helper_method :current_user
+    helper_method :current_user, :logged_in?, :require_login
 end
