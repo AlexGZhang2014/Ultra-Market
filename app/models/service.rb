d95ef2059540c843022cefc8245b198f96a6a991
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-22 21:13:16 -0400

    Add validations for name and description

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,8 @@
 class Service < ApplicationRecord
   belongs_to :merchant, optional: true
   belongs_to :client, optional: true
+  validates :name, presence: true
+  validates :description, presence: true
   scope :most_recent, -> { order("services.updated_at DESC") }
   scope :oldest, -> { order("services.updated_at ASC") }
   scope :available, -> { where(bought: false) }
