ca1e7a595147aeb6b28edef71f2798fccca7479a
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-22 22:44:21 -0400

    Modify so that most_recent and oldest routes for both item and service are defined before standard RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static#home"
   resources :user_accounts
+  get "/items/most_recent" => "items#most_recent"
+  get "/items/oldest" => "items#oldest"
+  get "/services/most_recent" => "services#most_recent"
+  get "/services/oldest" => "services#oldest"
   resources :merchants do
     resources :items, only: [:index, :new, :edit, :show]
     resources :services, only: [:index, :new, :edit, :show]
