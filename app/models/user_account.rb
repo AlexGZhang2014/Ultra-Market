95ada4db4f2164cad7d5576899e57b5552244796
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-22 21:09:56 -0400

    Define class method #create_with_omniauth so that user can log in with github

--- app/models/user_account.rb
+++ app/models/user_account.rb
@@ -4,4 +4,14 @@ class UserAccount < ApplicationRecord
   has_secure_password
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.username = auth["info"]["name"]
+      user.email = auth["info"]["email"]
+      user.password = auth["credentials"]["token"]
+    end
+  end
 end
