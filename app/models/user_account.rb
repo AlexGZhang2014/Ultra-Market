e5a110a9037cad95326cbb327d7ca54b123881b0
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-30 22:14:29 -0400

    Add email validation for correct email format

--- app/models/user_account.rb
+++ app/models/user_account.rb
@@ -3,7 +3,7 @@ class UserAccount < ApplicationRecord
   belongs_to :client, optional: true
   has_secure_password
   validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})\z/, on: :create }
 
   def self.create_with_omniauth(auth)
     create! do |user|
