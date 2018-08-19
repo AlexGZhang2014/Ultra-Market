09c0c76782a325d4cf8de833269cff932a80e903
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-08-18 22:34:59 -0400

    Refactor code to use ES6 JS class syntax

--- app/assets/javascripts/service.js
+++ app/assets/javascripts/service.js
@@ -1,75 +1,149 @@
-function Service(attributes) {
-  this.name = attributes.name;
-  this.description = attributes.description;
-  this.id = attributes.id;
-  this.bought = attributes.bought;
-  this.completed = attributes.completed;
-  this.merchant = attributes.merchant;
-}
-
-Service.submitCreateForm = function(e) {
-  e.preventDefault();
-  let $form_values = $(this).serialize();
-  $.post("/services", $form_values, Service.successCreate, "json");
-}
-
-Service.listClick = function(e) {
-  e.preventDefault();
-  $.get("/services.json", Service.successList);
-}
-
-Service.nextService = function() {
-  let nextId = parseInt($("#js-next-service").attr("data-id")) + 1;
-  $.get("/services/" + nextId + ".json", Service.successNextService);
-}
-
-Service.successCreate = function(data) {
-  let service = new Service(data);
-  return service.createService();
-}
-
-Service.successList = function(data) {
-  for (const i in data) {
-    let service = new Service(data[i]);
-    service.listService();
+class Service {
+  constructor(attributes) {
+    this.name = attributes.name;
+    this.description = attributes.description;
+    this.id = attributes.id;
+    this.bought = attributes.bought;
+    this.completed = attributes.completed;
+    this.merchant = attributes.merchant;
   }
-}
 
-Service.successNextService = function(data) {
-  let service = new Service(data);
-  return service.displayNextServiceData();
-}
+  static submitCreateForm(e) {
+    e.preventDefault();
+    let $form_values = $(this).serialize();
+    $.post("/services", $form_values, Service.successCreate, "json");
+  }
 
-Service.prototype.createService = function() {
-  $("#serviceName").text(this.name);
-  $("#serviceDescription").text(this.description);
-  $("#serviceMerchant").text(`Sold by: ${this.merchant.name}`);
-}
+  static listClick(e) {
+    e.preventDefault();
+    $.get("/services.json", Service.successList);
+  }
 
-Service.prototype.listService = function() {
-  $("#all-services").append(`
-    <h4>${this.name}</h4>
-    <p>${this.description}</p>
-    `);
-}
+  static nextService() {
+    let nextId = parseInt($("#js-next-service").attr("data-id")) + 1;
+    $.get("/services/" + nextId + ".json", Service.successNextService);
+  }
 
-Service.prototype.displayNextServiceData = function() {
-  $("#serviceName").text(this.name);
-  $("#serviceDescription").text(this.description);
-  $("#serviceMerchant").text(this.merchant.name);
-  $("#js-next-service").attr("data-id", this.id);
-}
+  static successCreate(data) {
+    let service = new Service(data);
+    return service.createService();
+  }
 
-Service.addJavaScriptListener = function() {
-  $("form#new_service").submit(Service.submitCreateForm);
-  $("#js-all-services").on("click", Service.listClick);
-  $("#js-next-service").on("click", Service.nextService);
-}
+  static successList(data) {
+    for (const i in data) {
+      let service = new Service(data[i]);
+      service.listService();
+    }
+  }
 
-Service.ready = function() {
-  Service.addJavaScriptListener();
+  static successNextService(data) {
+    let service = new Service(data);
+    return service.displayNextServiceData();
+  }
+
+  createService() {
+    $("#serviceName").text(this.name);
+    $("#serviceDescription").text(this.description);
+    $("#serviceMerchant").text(`Sold by: ${this.merchant.name}`);
+  }
+
+  listService() {
+    $("#all-services").append(`
+      <h4>${this.name}</h4>
+      <p>${this.description}</p>
+      `);
+  }
+
+  displayNextServiceData() {
+    $("#serviceName").text(this.name);
+    $("#serviceDescription").text(this.description);
+    $("#serviceMerchant").text(this.merchant.name);
+    $("#js-next-service").attr("data-id", this.id);
+  }
+
+  static addJavaScriptListener() {
+    $("form#new_service").submit(Service.submitCreateForm);
+    $("#js-all-services").on("click", Service.listClick);
+    $("#js-next-service").on("click", Service.nextService);
+  }
+
+  static ready() {
+    Service.addJavaScriptListener();
+  }
 }
 
 $(function() {
   Service.ready();
 });
+
+// function Service(attributes) {
+//   this.name = attributes.name;
+//   this.description = attributes.description;
+//   this.id = attributes.id;
+//   this.bought = attributes.bought;
+//   this.completed = attributes.completed;
+//   this.merchant = attributes.merchant;
+// }
+//
+// Service.submitCreateForm = function(e) {
+//   e.preventDefault();
+//   let $form_values = $(this).serialize();
+//   $.post("/services", $form_values, Service.successCreate, "json");
+// }
+//
+// Service.listClick = function(e) {
+//   e.preventDefault();
+//   $.get("/services.json", Service.successList);
+// }
+//
+// Service.nextService = function() {
+//   let nextId = parseInt($("#js-next-service").attr("data-id")) + 1;
+//   $.get("/services/" + nextId + ".json", Service.successNextService);
+// }
+//
+// Service.successCreate = function(data) {
+//   let service = new Service(data);
+//   return service.createService();
+// }
+//
+// Service.successList = function(data) {
+//   for (const i in data) {
+//     let service = new Service(data[i]);
+//     service.listService();
+//   }
+// }
+//
+// Service.successNextService = function(data) {
+//   let service = new Service(data);
+//   return service.displayNextServiceData();
+// }
+//
+// Service.prototype.createService = function() {
+//   $("#serviceName").text(this.name);
+//   $("#serviceDescription").text(this.description);
+//   $("#serviceMerchant").text(`Sold by: ${this.merchant.name}`);
+// }
+//
+// Service.prototype.listService = function() {
+//   $("#all-services").append(`
+//     <h4>${this.name}</h4>
+//     <p>${this.description}</p>
+//     `);
+// }
+//
+// Service.prototype.displayNextServiceData = function() {
+//   $("#serviceName").text(this.name);
+//   $("#serviceDescription").text(this.description);
+//   $("#serviceMerchant").text(this.merchant.name);
+//   $("#js-next-service").attr("data-id", this.id);
+// }
+//
+// Service.addJavaScriptListener = function() {
+//   $("form#new_service").submit(Service.submitCreateForm);
+//   $("#js-all-services").on("click", Service.listClick);
+//   $("#js-next-service").on("click", Service.nextService);
+// }
+//
+// Service.ready = function() {
+//   Service.addJavaScriptListener();
+// }
