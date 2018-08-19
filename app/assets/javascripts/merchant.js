66e7e4b8cc611539301b23192045db5989c74f5e
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-08-18 21:27:57 -0400

    Refactor code to allow both items and services to be displayed on the page

--- app/assets/javascripts/merchant.js
+++ app/assets/javascripts/merchant.js
@@ -2,6 +2,7 @@ function Merchant(attributes) {
   this.id = attributes.id;
   this.name = attributes.name;
   this.items = attributes.items;
+  this.services = attributes.services;
 }
 
 Merchant.listClick = function(e) {
@@ -13,20 +14,36 @@ Merchant.successList = function(data) {
   for (const m in data) {
     let merchant = new Merchant(data[m]);
     merchant.listMerchant();
-    merchant.listMerchantItems();
+    merchant.listItems();
+    merchant.listServices();
   }
 }
 
 Merchant.prototype.listMerchant = function() {
-  $("#all-merchants").append(`<h3>${this.name}</h3><br>`);
+  $("#all-merchants").append(`<h2>${this.name}</h2><br>`);
 }
 
-Merchant.prototype.listMerchantItems = function() {
+Merchant.prototype.listItems = function() {
   for (const i in this.items) {
+    if (i == 0) {
+      $("#all-merchants").append(`<h3>Items:</h3><br>`);
+    }
     $("#all-merchants").append(`
-      <h4>${this.items[i].name}</h4>
+      <h4>${parseInt(i)+1}. ${this.items[i].name}</h4>
       <p>${this.items[i].description}</p>
-      `)
+      `);
+  }
+}
+
+Merchant.prototype.listServices = function() {
+  for (const s in this.services) {
+    if (s == 0) {
+      $("#all-merchants").append(`<h3>Services:</h3><br>`);
+    }
+    $("#all-merchants").append(`
+      <h4>${parseInt(s)+1}. ${this.services[s].name}</h4>
+      <p>${this.services[s].description}</p>
+      `);
   }
 }
 
