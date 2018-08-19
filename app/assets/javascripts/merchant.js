c0ae690c619a56650d7c62ac1071c844c6bff15c
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-08-18 22:42:21 -0400

    Refactor code to use ES6 JS class syntax

--- app/assets/javascripts/merchant.js
+++ app/assets/javascripts/merchant.js
@@ -1,60 +1,119 @@
-function Merchant(attributes) {
-  this.id = attributes.id;
-  this.name = attributes.name;
-  this.items = attributes.items;
-  this.services = attributes.services;
-}
-
-Merchant.listClick = function(e) {
-  e.preventDefault();
-  $.get("/merchants.json", Merchant.successList);
-}
-
-Merchant.successList = function(data) {
-  for (const m in data) {
-    let merchant = new Merchant(data[m]);
-    merchant.listMerchant();
-    merchant.listItems();
-    merchant.listServices();
+class Merchant {
+  constructor(attributes) {
+    this.id = attributes.id;
+    this.name = attributes.name;
+    this.items = attributes.items;
+    this.services = attributes.services;
   }
-}
 
-Merchant.prototype.listMerchant = function() {
-  $("#all-merchants").append(`<h2>${this.name}</h2><br>`);
-}
+  static listClick(e) {
+    e.preventDefault();
+    $.get("/merchants.json", Merchant.successList);
+  }
 
-Merchant.prototype.listItems = function() {
-  for (const i in this.items) {
-    if (i == 0) {
-      $("#all-merchants").append(`<h3>Items:</h3><br>`);
+  static successList(data) {
+    for (const m in data) {
+      let merchant = new Merchant(data[m]);
+      merchant.listMerchant();
+      merchant.listItems();
+      merchant.listServices();
     }
-    $("#all-merchants").append(`
-      <h4>${parseInt(i)+1}. ${this.items[i].name}</h4>
-      <p>${this.items[i].description}</p>
-      `);
   }
-}
 
-Merchant.prototype.listServices = function() {
-  for (const s in this.services) {
-    if (s == 0) {
-      $("#all-merchants").append(`<h3>Services:</h3><br>`);
+  listMerchant() {
+    $("#all-merchants").append(`<h2>${this.name}</h2><br>`);
+  }
+
+  listItems() {
+    for (const i in this.items) {
+      if (i == 0) {
+        $("#all-merchants").append(`<h3>Items:</h3><br>`);
+      }
+      $("#all-merchants").append(`
+        <h4>${parseInt(i)+1}. ${this.items[i].name}</h4>
+        <p>${this.items[i].description}</p>
+        `);
     }
-    $("#all-merchants").append(`
-      <h4>${parseInt(s)+1}. ${this.services[s].name}</h4>
-      <p>${this.services[s].description}</p>
-      `);
   }
-}
 
-Merchant.addJavaScriptListener = function() {
-  $("#js-all-merchants").on("click", Merchant.listClick);
-}
+  listServices() {
+    for (const s in this.services) {
+      if (s == 0) {
+        $("#all-merchants").append(`<h3>Services:</h3><br>`);
+      }
+      $("#all-merchants").append(`
+        <h4>${parseInt(s)+1}. ${this.services[s].name}</h4>
+        <p>${this.services[s].description}</p>
+        `);
+    }
+  }
 
-Merchant.ready = function() {
-  Merchant.addJavaScriptListener();
+  static addJavaScriptListener() {
+    $("#js-all-merchants").on("click", Merchant.listClick);
+  }
+
+  static ready() {
+    Merchant.addJavaScriptListener();
+  }
 }
 
 $(function() {
   Merchant.ready();
 });
+
+// function Merchant(attributes) {
+//   this.id = attributes.id;
+//   this.name = attributes.name;
+//   this.items = attributes.items;
+//   this.services = attributes.services;
+// }
+
+// Merchant.listClick = function(e) {
+//   e.preventDefault();
+//   $.get("/merchants.json", Merchant.successList);
+// }
+
+// Merchant.successList = function(data) {
+//   for (const m in data) {
+//     let merchant = new Merchant(data[m]);
+//     merchant.listMerchant();
+//     merchant.listItems();
+//     merchant.listServices();
+//   }
+// }
+
+// Merchant.prototype.listMerchant = function() {
+//   $("#all-merchants").append(`<h2>${this.name}</h2><br>`);
+// }
+
+// Merchant.prototype.listItems = function() {
+//   for (const i in this.items) {
+//     if (i == 0) {
+//       $("#all-merchants").append(`<h3>Items:</h3><br>`);
+//     }
+//     $("#all-merchants").append(`
+//       <h4>${parseInt(i)+1}. ${this.items[i].name}</h4>
+//       <p>${this.items[i].description}</p>
+//       `);
+//   }
+// }
+
+// Merchant.prototype.listServices = function() {
+//   for (const s in this.services) {
+//     if (s == 0) {
+//       $("#all-merchants").append(`<h3>Services:</h3><br>`);
+//     }
+//     $("#all-merchants").append(`
+//       <h4>${parseInt(s)+1}. ${this.services[s].name}</h4>
+//       <p>${this.services[s].description}</p>
+//       `);
+//   }
+// }
+
+// Merchant.addJavaScriptListener = function() {
+//   $("#js-all-merchants").on("click", Merchant.listClick);
+// }
+
+// Merchant.ready = function() {
+//   Merchant.addJavaScriptListener();
+// }
